d6aa2910c8c461c60bb28da13c93e3c80b77d6e1
Rohit Chouhan <itsrohitofficial@gmail.com>
2025-09-29 00:32:38 +0530

    Migrate to GitHub Pages: remove hash routing, update homepage, simplify build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import './assets/bulma/css/bulma.min.css';
 import App from './App';
